683a6f0562c973193bb4e03621f627d4f0d2dd30
abeer-mohamed <abeerelghool@gmail.com>
2021-07-08 05:42:33 +0200

    upload file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include('index.html');
 
-if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['url'])){
+if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['url']) && isset($_FILES['formFile'])){
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         die("Invalid email format");
     }
@@ -10,6 +10,14 @@ if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['url'])){
     }
     if (!filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
         die($_POST['url']." is not a valid URL");
+    }
+    if ($_FILES['formFile']['type']=="application/pdf" || $_FILES['formFile']['type']=="image/gif" || $_FILES['formFile']['type']=="image/jpeg") {
+
+        if (move_uploaded_file($_FILES['formFile']['tmp_name'], "C:\\xampp\\htdocs\\untitled\\cv\\".$_FILES['formFile']['name'])) {
+
+            echo " The file " . basename($_FILES['formFile']['name']) . "  is uploaded <br>";
+
+        }
     }
     echo "Success";
 }else{
